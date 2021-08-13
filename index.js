52a099d68cf7c5a53e98ecbd0605663847aac1fa
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-08-12 20:53:10 -0400

    data added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv").config() //eslint-disable-line
 const express = require("express") //eslint-disable-line
 
 const cors = require("cors") //eslint-disable-line
-
+const importData = require("./data.json")//eslint-disable-line
 const app = express()
 const PORT = process.env.PORT||3000 //eslint-disable-line
 
@@ -13,10 +13,13 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get("/api/*", (_,res)=>{
+app.get("/", (req,res)=>{
     res.json({data: "The API is working"})
 })
 
+app.get("/users", (req, res)=>{
+    res.send(importData)
+})
 app.use((err, req, res, next)=>{ //eslint-disable-line
 
     res.status(500).json({
